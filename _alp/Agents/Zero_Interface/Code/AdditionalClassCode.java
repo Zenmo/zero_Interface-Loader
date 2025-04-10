00f2d9ac75362e74ef8f4438672b0eee71da6717
Ate Hempenius <ate@zenmo.com>
2025-04-10 12:09:56 +0200

    Added getters for filter selection buttons

--- _alp/Agents/Zero_Interface/Code/AdditionalClassCode.java
+++ _alp/Agents/Zero_Interface/Code/AdditionalClassCode.java
@@ -4,3 +4,9 @@ public ShapeButton getButton_goToUI() {
 public ShapeRadioButtonGroup getBuildingColorsRB(){
 	return this.rb_buildingColors;
 }
+public ShapeCheckBox getCb_showFilterInterface(){
+	return this.cb_showFilterInterface;
+}
+public ShapeComboBox getCb_filterOptions(){
+	return this.cb_filterOptions;
+}
