f2e16451120ef6405da967c41a952f6000938346
Ate Hempenius <ate@zenmo.com>
2025-06-24 17:32:01 +0200

    adjusting default visibility of HVMV node+ making amount of chargepoints optional for charging poles (still mandatory for centres)

--- _alp/Classes/Class.Chargingstation_data.java
+++ _alp/Classes/Class.Chargingstation_data.java
@@ -19,7 +19,7 @@ public class Chargingstation_data {
 	Double connection_capacity_kw;	
 	boolean is_charging_centre;
 	OL_EnergyAssetType vehicle_type;
-	int number_of_chargers;
+	Integer number_of_chargers;
 	double power_per_charger_kw;
 	boolean initially_active;
 	double latitude;
