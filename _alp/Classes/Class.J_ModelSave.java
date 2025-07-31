be0e6792cf0d78c898d74d21ae514aac66c1ae38
GillisHommen <98752265+GillisHommen@users.noreply.github.com>
2025-07-31 15:15:56 +0200

    Serialisation tweaks (#133)
    * serialisation of J_ModelSave (containing EnergyModel and a few other collections) instead of energyModel itself.
    
    * Deserialisation without reflection hack
    
    Still some inconsitency in EV charging behavior before/after serialisation.

---
+++ _alp/Classes/Class.J_ModelSave.java
@@ -0,0 +1,41 @@
+/**
+ * J_ModelSave
+ */	
+public class J_ModelSave implements Serializable {
+	
+	public EnergyModel energyModel;
+	public ArrayList<GridNode> c_gridNodes = new ArrayList<GridNode>();
+	public ArrayList<GIS_Object> c_GISObjects = new ArrayList<GIS_Object>();
+	
+	public ArrayList<GCUtility> c_orderedPVSystemsCompanies;
+	public ArrayList<GCHouse> c_orderedPVSystemsHouses;
+	public ArrayList<J_EA> c_orderedVehicles;
+	public ArrayList<GCUtility> c_orderedHeatingSystemsCompanies;
+	public ArrayList<GCHouse> c_orderedHeatingSystemsHouses;
+	public ArrayList<J_EADieselVehicle> c_orderedActiveVehiclesPublicParking;
+	public ArrayList<J_EADieselVehicle> c_orderedNonActiveVehiclesPublicParking;
+	public ArrayList<J_EAVehicle> c_orderedVehiclesPrivateParking;
+	public ArrayList<GIS_Object> c_orderedParkingSpaces;
+	
+	public ArrayList<J_EACharger> c_orderedV1GChargers;
+	public ArrayList<J_EACharger> c_orderedV2GChargers;
+	public ArrayList<GCPublicCharger> c_orderedPublicChargers;
+
+    /**
+     * Default constructor
+     */
+    public J_ModelSave() {
+    }
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
+	/**
+	 * This number is here for model snapshot storing purpose<br>
+	 * It needs to be changed when this class gets changed
+	 */ 
+	private static final long serialVersionUID = 1L;
+
+}
